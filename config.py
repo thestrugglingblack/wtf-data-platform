e2ad552363dff752f207b82cd99c4780a43c4371
thestrugglingblack <zurihunter92@gmail.com>
2026-09-07 21:58:32 -0400

    add: Updated config with paths for the release of the data

--- config.py
+++ config.py
@@ -9,6 +9,16 @@ DATA_DIR = Path(__file__).parent / "data"  # Directory to store data files
 RAW_DATA_DIR = DATA_DIR / "raw"  # Directory for raw data files
 PROCESSED_DATA_DIR = DATA_DIR / "processed"  # Directory for processed data files
 
+# Data Product Paths
+# Example:
+#
+# data/releases/
+#     v0.1.0/
+#     v0.2.0/
+RELEASES_DATA_DIR = DATA_DIR / "releases"
+LATEST_RELEASE_FILE = RELEASES_DATA_DIR / "latest.json"
+
+
 LEAGUES= ['wfa', 'wnfc']
 SEASONS = {
     "wfa": [2009, 2010, 2011, 2012, 2013, 2014, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026], # [2020, 2021, 2022, 2023, 2024, 2025, 2026] 2009, 2010, 2011, 2012, 2013, 2014, 2017, 2018, 2019
